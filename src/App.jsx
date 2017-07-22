338ee298ad462958d2337b6d8eec78e0dccd52e0
nursh <nsheikhmohammed@yahoo.com>
2017-07-22 06:41:07 -0400

    Made Workout Table frame with some test JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 
+import Table from './WorkoutTable.jsx';
+
 class App extends Component {
 
+  state = {
+    
+  }
 
   render() {
     return (
       <div className="ui center aligned header">
         <h1>Workout Log</h1>
+        <Table />
       </div>
     );
   }
---
+++ src/WorkoutTable.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Table() {
+  return (
+    <table className="ui celled structured table">
+      <thead>
+        <tr>
+          <th>Body Target</th>
+          <th>Activity</th>
+          <th>Sets</th>
+          <th>Reps</th>
+          <th>Weight</th>
+          <th>Time</th>
+          <th>Distance</th>
+        </tr>
+      </thead>
+    </table>
+  );
+}
+
+export default Table;
